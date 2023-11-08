2948bed34d18412e122dfbfa200e0abb63729ffd
mmors <m.morski@gmail.com>
2023-11-08 12:57:36 +0100

    Initial commit

---
+++ src/main/java/com/morski/proofit/bicyclepolicy/BicyclepolicyApplication.java
@@ -0,0 +1,13 @@
+package com.morski.proofit.bicyclepolicy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicyclepolicyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicyclepolicyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/morski/proofit/bicyclepolicy/BicyclepolicyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morski.proofit.bicyclepolicy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicyclepolicyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
